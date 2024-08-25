7a96086a23413a9850985bc457d0c54140036eec
Ujjwala <ujjwalap@udel.edu>
2024-08-25 19:25:35 -0400

    changed firebase

--- firebase.js
+++ firebase.js
@@ -6,15 +6,16 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
-    apiKey: "AIzaSyB-gss4J49R_6fGrPzoNw9ElnSJfvr1tfc",
-    authDomain: "pantry-tracker-ff841.firebaseapp.com",
-    projectId: "pantry-tracker-ff841",
-    storageBucket: "pantry-tracker-ff841.appspot.com",
-    messagingSenderId: "305077232177",
-    appId: "1:305077232177:web:d5796c28a9af3d5b17eb10",
-    measurementId: "G-QS0WC0DKL0"
-};
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
